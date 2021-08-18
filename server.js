ebb8b95bf3d492baa00f9632ee10c5a024388cd6
Bach Duong Kinh <kinhbdps11658@fpt.edu.vn>
2021-08-18 23:46:24 +0700

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./src/data/db.json");
+const router = jsonServer.router("./data/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
