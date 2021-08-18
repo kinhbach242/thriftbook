94dd36e73c1c1013eec621a30b2c028959bd6213
Bach Duong Kinh <kinhbdps11658@fpt.edu.vn>
2021-08-19 00:07:13 +0700

    update

--- src/components/listItem/listItem.js
+++ src/components/listItem/listItem.js
@@ -1,29 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import Item from '../item/Item';
+import React, { useEffect, useState } from "react";
+import Item from "../item/Item";
 function ListItem() {
-    const [items, setItems] = useState([]);
-    useEffect(() =>{
-        fetch("http://localhost:3000/books")
-        .then(res=> res.json())
-        .then(
-            (data) =>{
-                setItems(data)
-            }
-        )
-    })
-    const showList = () =>{
-       let html = <div className="page-products-items">
-             {items.map(book =>{
-                 return (<Item key={book.id} book={book}/>)
-             })}
-             </div>
-        return html;
-    }
-    return (
-        
-        showList()
-      
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://thriftbook.herokuapp.com/api/books")
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      });
+  });
+  const showList = () => {
+    let html = (
+      <div className="page-products-items">
+        {items.map((book) => {
+          return <Item key={book.id} book={book} />;
+        })}
+      </div>
     );
+    return html;
+  };
+  return showList();
 }
 
 export default ListItem;
